26639d6da68793a0f076c6646e975f130062fba4
kalyzca <alcantarakaly@gmail.com>
2022-07-20 20:16:36 -0500

    creando el componente login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ]
 })
 export class LoginModule { }
